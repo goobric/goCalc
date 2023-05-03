cd49526bf83ac13c55795ac9087206daabfa0956
goobric <goobric@gmail.com>
2023-05-04 03:20:21 +0800

    created a struct with 3 instances to loop over for Divide func

--- calculator_test.go
+++ calculator_test.go
@@ -77,11 +77,30 @@ func TestMultiply(t *testing.T) {
 	}
 	testCases := []testCase{
 		{a: 2, b: 0.5, want: 1},
-		{a: 7, b: 2, want: 14},
+		{a: -7, b: 2, want: -14},
 		{a: 5.5, b: 3, want: 16.5},
 	}
 	for _, tc := range testCases {
 		got := calculator.Multiply(tc.a, tc.b)
+		it tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 2, want: 3},
+		{a: 5, b: 1, want: 6},
+	}
+	for _, tc := range testCases {
+		got := calculator.Divide(tc.a, tc.b)
 		it tc.want != got {
 			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
 		}
