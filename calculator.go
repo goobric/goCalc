3a59faaa33d19dedea063914fcb46e61e9b6092f
goobric <goobric@gmail.com>
2023-05-04 17:42:40 +0800

    created the Sqrt func

--- calculator.go
+++ calculator.go
@@ -1,7 +1,11 @@
 // Package calculator does simple calculations
 package calculator
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
 // func Add takes two numbers and returns the result of adding them together
 func Add(a, b float64) float64 {
@@ -24,4 +28,12 @@ func Divide(a, b float64) (float64, error) {
 		return 0, errors.New("division by zero is not allowed")
 	}
 	return a / b, nil
+}
+
+// Sqrt func returns the square root of the input number, or an error for negative inputs.
+func Sqrt(input float64) (float64, error) {
+	if input < 0 {
+		return 0, fmt.Errorf("square root of negative number is not allowed: %f", input)
+	}
+	return math.Sqrt(input), nil
 }
